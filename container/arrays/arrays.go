5d7dafc9ab4d6e8e2ffcd6265aa48f68a5c22ae5
litiaohua <tiaohuali@qq.com>
2019-06-23 11:04:44 +0800

    添加 数组的使用

---
+++ container/arrays/arrays.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printArray(arr [5]int) {
+	// [5]int. [3]int是不同的类型
+	arr[0] = 100
+	for i, v := range arr{
+		fmt.Println(i, v)
+	}
+}
+
+func main() {
+	var arr1 [5]int
+	arr2 := [3]int{1, 3, 5}
+	//[...]让编译器来数有几个int
+	arr3 := [...]int{2, 4, 6, 8, 10}
+	var grid [4][5]int
+
+	fmt.Println("array definitions:")
+	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(grid)
+
+	fmt.Println("printArray(arr1)")
+	printArray(arr1)
+
+	fmt.Println("printArray(arr3)")
+	printArray(arr3)
+
+	fmt.Println("arr1 and arr3")
+	fmt.Println(arr1, arr3)
+}
