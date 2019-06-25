dd681c4a0ea6f87a4bcefad2d2d64f10a08140ac
litiaohua <tiaohuali@qq.com>
2019-06-25 14:51:11 +0800

    修改 修改Queue使队列适用于任何类型的数据

--- queue/queue.go
+++ queue/queue.go
@@ -5,16 +5,32 @@ package queue
  */
 // A FIFO queue.
 // Queue就是一个int的silce
-type Queue []int
+
+/**
+	将int改为interface后，该Queue支持任何类型
+ */
+type Queue []interface{}
 
 // Pushes the element into the queue.
 // 		e.g. q.Push(123)
-func (q *Queue) Push(v int) {
+/**
+	该Queue支持所有类型，但是Push限定为int类型，把Push的入参改为int类型即可
+	或者是*q = append(*q, v.(int))
+ */
+func (q *Queue) Push(v interface{}) {
 	*q = append(*q, v)
 }
 
 // Pops element from head.
-func (q *Queue) Pop() int {
+/**
+	该Queue支持所有类型，但是Pop限定为int类型，在head后面.(int)
+ */
+//func (q *Queue) Pop() int {
+//	head := (*q)[0]
+//	*q = (*q)[1:]
+//	return head.(int)
+//}
+func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head
