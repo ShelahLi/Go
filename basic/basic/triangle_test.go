26dc1d96d19275e61c92fb64e440286dab3a896c
litiaohua <tiaohuali@qq.com>
2019-06-27 14:35:39 +0800

    添加 go中普通测试的使用方法

--- basic/basic/triangle_test.go
+++ basic/basic/triangle_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestTriangle(t *testing.T) {
+
+	tests := []struct{
+		a, b, c int
+	}{
+		{3,4,5},
+		{6,8,10},
+		{1,2,3},
+		{30000,40000,50000},
+	}
+
+	for _, tt := range tests{
+		actual := calcTriangle(tt.a, tt.b)
+		if actual != tt.c{
+			/**
+				用t.Errorf()输出错误信息
+			 */
+			t.Errorf("%d^2 + %d^2 != %d^2", tt.a, tt.b, tt.c)
+		}
+	}
+
+}
---
+++ container/nonrepeatingsubstr/nonrepeating_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+/**
+	testing 提供对 Go 包的自动化测试的支持。通过 `go test` 命令，能够自动执行如下形式的任何函数：
+	func TestXxx(*testing.T)
+	其中 Xxx 可以是任何字母数字字符串（但第一个字母不能是 [a-z]），用于识别测试例程。
+	在这些函数中，使用 Error, Fail 或相关方法来发出失败信号。
+*/
+func TestSubstr(t *testing.T) {
+	/**
+		表格驱动测试：分离了测试数据与测试逻辑、明确了出错信息
+	 */
+	tests := []struct {
+		s   string
+		ans int
+	}{
+		// Normal cases
+		{"abcabcbb", 3},
+		{"pwwkew", 3},
+
+		// Edge cases
+		{"", 0},
+		{"b", 1},
+		{"bbbbbbbbb", 1},
+		{"abcabcabcd", 4},
+
+		// Chinese support
+		{"这里是北京邮电大学", 9},
+		{"一二三二一", 3},
+		{"黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花", 8},
+	}
+
+	for _, tt := range tests {
+		actual := lengthOfNonRepeatingSubStr(tt.s)
+		if actual != tt.ans {t.Errorf("got %d for input %s; expected %d", actual, tt.s, tt.ans)
+		}
+	}
+
+}
+
+
